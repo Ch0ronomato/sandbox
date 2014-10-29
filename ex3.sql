da0284ece59cab2db99d75933eb6ab852b0dc5a6
Sebastian Duchene <sebastianducheneMacBook@vlan-2640-10-19-72-192.student.wireless.sydney.edu.au>
2014-10-29 13:53:04 +1100

    added ex5

---
+++ ex3.sql
@@ -0,0 +1,7 @@
+INSERT INTO person (id, first_name, last_name, age)
+       VALUES (0, 'Sebastian', 'Duchene', 30);
+
+INSERT INTO pet (id, name, breed, age, dead)
+       VALUES (0, 'fluffy', 'unicorn', 1000, 0);
+
+INSERT INTO pet VALUES (1, 'GIGANTOR', 'ROBOT', 1, 1);
