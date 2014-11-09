00cea5764bddcb1ec42de57f75dcc2a5a9c24c52
sebastianduchene <sebastian.duchene@sydney.edu.au>
2014-11-09 23:45:12 +1100

    modified:   ex3.db 	modified:   ex6.sql 	modified:   ex6.sql~

--- ex6.sql
+++ ex6.sql
@@ -1,6 +1,4 @@
-SELECT pet.id, pet.name, pet.age, pet.dead
-       FROM pet, person_pet, person
-       WHERE
-       pet.id = person_pet.pet_id AND 
-       person_pet.person_id = person.id AND
-       person.first_name = 'Sebastian';
+SELECT pet.id, pet.name, pet.age, pet.dead FROM pet, person_pet, person
+WHERE person.first_name='Sebastian' AND 
+person_pet.person_id=person.id AND
+pet.id=person_pet.pet_id;
